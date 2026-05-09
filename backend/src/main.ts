9b37796071a207f14103b54cb220d25a223c43c4
josinon <josinon@gmail.com>
2026-05-09 18:48:51 -0300

    fix application startup to correct order of parameters in listen method for improved clarity

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -58,7 +58,7 @@ async function bootstrap() {
   app.use(new CsrfMiddleware().use.bind(new CsrfMiddleware()));
 
 
-  await app.listen('0.0.0.0', Number(process.env.PORT ?? 3001));
+  await app.listen(Number(process.env.PORT ?? 3001), '0.0.0.0');
 }
 
 bootstrap();
